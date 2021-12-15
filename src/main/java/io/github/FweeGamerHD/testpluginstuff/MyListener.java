b587e6b1f985267188e4b0b3689938bc331ad3fe
FweeGamerHD <freegamerhd12@gmail.com>
2021-12-15 21:34:01 +0100

    -forgot about the actual event itself, god I'm dumb

--- src/main/java/io/github/FweeGamerHD/testpluginstuff/MyListener.java
+++ src/main/java/io/github/FweeGamerHD/testpluginstuff/MyListener.java
@@ -14,6 +14,6 @@ public class MyListener implements Listener {
     }
 
     public void onExampleEvent(ExampleEvent event) {
-
+        Bukkit.broadcastMessage("Heyho, this is just a test message");
     }
 }
