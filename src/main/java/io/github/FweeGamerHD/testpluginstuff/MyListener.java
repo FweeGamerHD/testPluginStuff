3128b13e1d42f4e49a5693274e216817ea77daf6
FweeGamerHD <freegamerhd12@gmail.com>
2021-12-15 21:32:09 +0100

    -added events, one for joining and one for writing something in the chat

---
+++ src/main/java/io/github/FweeGamerHD/testpluginstuff/MyListener.java
@@ -0,0 +1,19 @@
+package io.github.FweeGamerHD.testpluginstuff;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class MyListener implements Listener {
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
+        event.setJoinMessage(ChatColor.AQUA + "[" + ChatColor.RED + "+" + ChatColor.AQUA + "]" + event.getPlayer().getName());
+    }
+
+    public void onExampleEvent(ExampleEvent event) {
+
+    }
+}
--- src/main/java/io/github/FweeGamerHD/testpluginstuff/TestPluginStuff.java
+++ src/main/java/io/github/FweeGamerHD/testpluginstuff/TestPluginStuff.java
@@ -1,12 +1,22 @@
 package io.github.FweeGamerHD.testpluginstuff;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TestPluginStuff extends JavaPlugin {
     @Override
     public void onEnable(){
         getLogger().info("onEnable has been called!");
+
+        getServer().getPluginManager().registerEvents(new MyListener(), this);
+
         this.getCommand("kit").setExecutor(new CommandKit());
+
+        ExampleEvent exampleEvent = new ExampleEvent("Msrules123");
+        Bukkit.getPluginManager().callEvent(exampleEvent);
+        if (!exampleEvent.isCancelled()) {
+            Bukkit.getPlayer("Msrules123").sendMessage(exampleEvent.getPlayerName());
+        }
     }
     @Override
     public void onDisable(){
